d4eaf44d37460816624dfb1d9f40651b92b46453
thony <a0982830615@gmail.com>
2022-10-11 22:47:27 +0800

    add

--- SignalRChat/wwwroot/js/UserCount.js
+++ SignalRChat/wwwroot/js/UserCount.js
@@ -5,14 +5,7 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/UserCountHub").bui
 //Disable the send button until connection is established.
 document.getElementById("sendButton").disabled = true;
 
-//connection.on("ReceiveProduct", function (user, message) {
-//    var li = document.createElement("li");
-//    document.getElementById("messagesList").appendChild(li);
-//    // We can assign user-supplied strings to an element's textContent because it
-//    // is not interpreted as markup. If you're assigning in any other way, you 
-//    // should be aware of possible script injection concerns.
-//    li.textContent = `${user} says ${message}`;
-//});
+
 
 connection.start().then(function () {
     document.getElementById("sendButton").disabled = false;
